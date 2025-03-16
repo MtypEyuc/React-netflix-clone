74a16e4b4e6df8391f30cc56eba2d4f0a7ea3339
dud <Heo250113@gmail.com>
2025-03-16 18:02:30 +0900

    feat: 슬라이드 기능 추가(2025-03-16) - 기간: 2025-03-16 - 내용: 따라하며 배우는 리액트 A-Z[19버전 반영] 슬라이드 기능 추가하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -20,7 +20,9 @@ function Row({ isLargeRow, title, id, fetchUrl }) {
             <h2>{title}</h2>
             <div className="slider">
                 <div className="slider__arrow-left">
-                    <span className="arrow">{"<"}</span>
+                    <span className="arrow" onClick={()=> {
+                        document.getElementById(id).scrollLeft -= window.innerWidth - 80
+                    }}>{"<"}</span>
                 </div>
              <div id={id} className="row__posters">
                  {movies.map(movie => (
@@ -33,7 +35,9 @@ function Row({ isLargeRow, title, id, fetchUrl }) {
                      />
                  ))}
              </div>
-                <div className="slider__arrow-right">
+                <div className="slider__arrow-right" onClick={()=>{
+                    document.getElementById(id).scrollLeft += window.innerWidth - 80
+                }}>
                     <span className="arrow">{">"}</span>
                 </div>
             </div>
