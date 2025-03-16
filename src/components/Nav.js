6b1e54886ee792df1691440db98f1dddc4063b83
dud <Heo250113@gmail.com>
2025-03-16 18:51:19 +0900

    feat: 검색 기능 구현(2025-03-16) - 기간: 2025-03-16 - 내용: 따라하며 배우는 리액트 A-Z[19버전 반영] UseLocation을 이용한 검색 페이지 구현하기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
-function Nav() {
+export default function Nav() {
     const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const navigate = useNavigate();
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
+            console.log("window.scrollY", window.scrollY);
             if (window.scrollY > 50) {
                 setShow(true);
             } else {
@@ -18,14 +22,28 @@ function Nav() {
         };
     }, []);
 
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+    };
+
     return (
         <nav className={`nav ${show && "nav__black"} `}>
             <img
                 alt="Netflix logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
                 className="nav__logo"
-                onClick={() => (window.location.reload())}
+                onClick={() => (window.location.href = "/")}
             />
+
+            <input
+                value={searchValue}
+                onChange={handleChange}
+                className="nav__input"
+                type="text"
+                placeholder="영화를 검색해주세요."
+            />
+
             <img
                 alt="User logged"
                 src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
@@ -34,5 +52,3 @@ function Nav() {
         </nav>
     );
 }
-
-export default Nav;
