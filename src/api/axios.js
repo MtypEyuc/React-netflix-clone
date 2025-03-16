e04cb6ad129e1af36c9a94c613fa834f3d0b57e8
dud <Heo250113@gmail.com>
2025-03-16 15:54:41 +0900

    design: 베너 컴포넌트 생성 및 css 생성(2025-03-16) - 기간: 2025-03-16 - 내용: 따라하며 배우는 리액트 A-Z[19버전 반영] 이미지 배너 생성하기

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
+
+    headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`,
+        accept: "application/json",
+    },
+
     params: {
         api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR",
